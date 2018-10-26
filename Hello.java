670c474c8c322d6ee2a4c6f88bb054c338dbd2f0
Mnorona <44488105+Mnorona@users.noreply.github.com>
2018-10-25 21:54:05 -0700

    hvkj

---
+++ Hello.java
@@ -0,0 +1,10 @@
+//This is my first program in Java
+import java.util.Scanner;
+public class Hello 
+{
+ public static void main(String args [])
+ {
+     
+ System.out.println("We're going to   the World Series");    
+ }
+}
